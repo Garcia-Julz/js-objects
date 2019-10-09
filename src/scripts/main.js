3086664ec7494e35a68f28e33a8985e9e083def1
Julian Garcia <garcia.j.julian@gmail.com>
2019-10-09 15:11:21 -0500

    lightning ex wardrobe

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,36 +1,51 @@
-console.log("We're learning about objects!")
+// console.log("We're learning about objects!")
 
-const car = {
-    year: 2015,
-    make: "Ford",
-    model: "Mustang",
-    color: "Red",
+// const car = {
+//     year: 2015,
+//     make: "Ford",
+//     model: "Mustang",
+//     color: "Red",
+// }
+
+// const Animals = ["Kippers", "Jack", "Gypsy", "Angus", "Seymour Bouts", "Sharky"]
+
+// const mom = {
+//     gender: "female",
+//     age: "44",
+// }
+
+// const dad = {
+//     gender: "female",
+//     age: "49",
+// }
+
+// const sister = {
+//     gender: "female",
+//     age: "14",
+// }
+
+// const brother = {
+//     gender: "male",
+//     age: "20",
+// }
+
+// const familyMembers = [mom, dad, sister, brother,]
+
+// console.log (
+//     "familyMembers", familyMembers
+// )
+
+const wardrobe = {
+    height: 80,
+    manufacturer: "Killibrew & Sons",
+    contents: ["Dress shirt", "Jeans", "Suit", "Skirt", "Tennis shoes"],
+    depth: 38,
+    width: 50,
 }
 
-const Animals = ["Kippers", "Jack", "Gypsy", "Angus", "Seymour Bouts", "Sharky"]
-
-const mom = {
-    gender: "female",
-    age: "44",
-}
-
-const dad = {
-    gender: "female",
-    age: "49",
-}
-
-const sister = {
-    gender: "female",
-    age: "14",
-}
-
-const brother = {
-    gender: "male",
-    age: "20",
-}
-
-const familyMembers = [mom, dad, sister, brother,]
-
-console.log (
-    "familyMembers", familyMembers
-)
+console.log("The wardrobe")
+console.log("wardrobe height", wardrobe.height)
+console.log("wardrobe depth", wardrobe.depth)
+console.log("wardrobe width" ,wardrobe.width)
+console.log("wardrobe manufacture", wardrobe.manufacturer)
+console.log(wardrobe.contents[0], wardrobe.contents[1], wardrobe.contents[2], wardrobe.contents[3], wardrobe.contents[4],)
