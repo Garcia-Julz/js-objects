f69e1155a3d3dd4f537319d215de50c290df0d5a
Julian Garcia <garcia.j.julian@gmail.com>
2019-10-10 11:32:40 -0500

    js-practice on ex-4

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -117,3 +117,40 @@ for (let key in instructorsObj) {
 
 console.log ("Andy: ", fullTimeInstructors[fullTimeInstructors.length -1])
 console.log ("Zoe: ", nashvilleSoftwareSchool.instructors.partTime[0])
+
+
+const beatles = {
+    albums: ["Abbey Road", "Sgt Peppers Lonely Heart's Club Band", "Revolver", "Magical Mystery Tour", "Something New"],
+    history: {
+        formed: 1960,
+        disbanded: 1970
+    },
+    members: [
+        {
+            name: "George Harrison",
+            birth: 1943,
+            death: 2001
+        },
+        {
+            name: "Paul McCartney",
+            birth: 1942,
+            death: null
+        },
+        {
+            name: "John Lennon",
+            birth: 1940,
+            death: 1980
+        },
+        {
+            name: "Ringo Starr",
+            birth: 1940,
+            death: null
+        }
+    ]
+}
+
+const mmt = beatles.albums[3]
+const paul = beatles.members[1].name
+
+
+console.log(`${paul} was in the Beatles ${beatles.history.formed} to ${beatles.history.disbanded}. He was born in ${beatles.members[1].birth}. He contributed heavily to the ${mmt} Album.`)
